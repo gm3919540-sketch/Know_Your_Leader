099897a1555f7ab4abcca13b95f3e31c83e1f9ae
gm3919540-sketch <gm3919540@gmail.com>
2026-03-19 21:52:05 +0530

    implemented spring ai with my projhect it replaces manual work in my project

--- MyNewProject/src/main/java/com/example/MyNewProject/service/AiService.java
+++ MyNewProject/src/main/java/com/example/MyNewProject/service/AiService.java
@@ -1,6 +1,7 @@
 package com.example.MyNewProject.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -18,58 +19,19 @@ import java.util.Map;
 @Service
 public class AiService {
 
-    @Value("${gemini.api.key}")
-    private String apiKey;
-    @Value("${gemini.api.url}")
-    private String apiUrl;
+    private  ChatClient chatClient;
+    public AiService(ChatClient.Builder builder){
+        this.chatClient =builder.build();
+    }
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    public String generateSummary(String prompt) throws Exception {
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        String body = """
-    {
-      "contents": [
-        {
-          "parts": [
-            { "text": "%s" }
-          ]
+    public String generateSummary(String propt) {
+        String response="";
+        try{
+             response = chatClient.prompt(propt).call().content();
+        } catch (Exception e) {
+            throw new RuntimeException("Getting Error in Fetching details with help of ai");
         }
-      ]
+        return response;
     }
-    """.formatted(prompt);
-
-        HttpEntity<String> entity = new HttpEntity<>(body, headers);
-
-        String finalUrl = apiUrl + "?key=" + apiKey;
-
-        ResponseEntity<String> response =
-                restTemplate.postForEntity(finalUrl, entity, String.class);
-
-        String responseBody = response.getBody();
-        System.out.println(responseBody);
-
-        //  Parse JSON
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> json = mapper.readValue(responseBody, Map.class);
-
-        List<Map<String, Object>> candidates =
-                (List<Map<String, Object>>) json.get("candidates");
-
-        Map<String, Object> firstCandidate = candidates.get(0);
-
-        Map<String, Object> content =
-                (Map<String, Object>) firstCandidate.get("content");
-
-        List<Map<String, Object>> parts =
-                (List<Map<String, Object>>) content.get("parts");
-
-        Map<String, Object> firstPart = parts.get(0);
-
-        return firstPart.get("text").toString();
-    }
-
-
 }
